fbe24c2c635c5cb60f5513caca0d4c75ac669f74
sfrisson <37748714+sfrisson@users.noreply.github.com>
2019-02-23 18:56:42 +0100

    Cleaned up version -useful

--- rsync_folder.sh
+++ rsync_folder.sh
@@ -5,37 +5,28 @@
 
 if [ -f ~/Downloads/*.mkv ]; #if any files .mkv
 clear 
-echo ""
-echo "Transfer ede fichier....."
-echo ""
-echo "========================================================="
-echo ""
+	echo ""
+	echo "Transfer ede fichier....."
+	echo ""
+	echo "========================================================="
+	echo ""
+
+
 then #then rsync it compressed with progress bar
-	rsync --compress --progress ~/Downloads/*.mkv /mnt/steph_filmes/ #&& rm -rfv ~/Downloads/*.mkv 
+	rsync --compress --progress ~/Downloads/*.mkv ~/filmes/ && rm -rfv ~/Downloads/*.mkv 
 
-	 #scp -c ~/Downloads/*.mkv ~/filmes/ && rm -rfv ~/Downloads/*.mkv 
-
-#&& above and auto rempve de source files
+#&& above and auto remove de source files
 		echo ""
 		echo "==========================================================" 
 		echo ""
 		echo " Autonettoyage complet-->> fichier dans dossier filmes !!!"
 		echo ""
-#clear
-
-#echo "=========================================================="
-#echo "transfer de fichier .avi"
-#echo "==========================================================="
-#echo ""
-
-	
-#	 rsync --compress --progress ~/Downloads/*.avi ~/filmes/ && rm -rfv ~/Downloads/*.avi 
-
+clear
 
 
 else # if not echo no file found exit 
-echo "====================="
-echo " Pas de fichier .mkv "
-echo "====================="
+	echo "====================="
+	echo " Pas de fichier .mkv "
+	echo "====================="
 
 fi 
